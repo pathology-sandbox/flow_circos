f6028e97272ee63a084ff3849d4e95ab07c20497
gcampuzano14 <gcampuzano14@outlook.com>
2017-08-08 22:41:34 -0500

    [master] Implement requirements script to install dependencies

---
+++ requirements.R
@@ -0,0 +1,18 @@
+# CRAN Pacakges
+# install_pak function: install and load multiple R packages.
+install_pak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, 'Package'])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+} 
+
+packages <- c('circlize')
+install_pak(packages)
+
+
+# BIOCONDUCTOR packages
+source('https://bioconductor.org/biocLite.R')
+biocLite("flowCore")
+biocLite("flowQ")
+biocLite("flowViz")

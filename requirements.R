db988bcd8c432448e5ee8581cde08a1429372d9b
gcampuzano14 <gcampuzano14@outlook.com>
2017-08-08 23:07:56 -0500

    [linting] General linting

--- requirements.R
+++ requirements.R
@@ -1,18 +1,19 @@
 # CRAN Pacakges
 # install_pak function: install and load multiple R packages.
 install_pak <- function(pkg){
-  new.pkg <- pkg[!(pkg %in% installed.packages()[, 'Package'])]
-  if (length(new.pkg)) 
-    install.packages(new.pkg, dependencies = TRUE)
-  sapply(pkg, require, character.only = TRUE)
-} 
+  new_pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) {
+    install.packages(new_pkg, dependencies = TRUE)
+    sapply(pkg, require, character.only = TRUE)
+  }
+}
 
-packages <- c('circlize')
+packages <- c("circlize")
 install_pak(packages)
 
 
 # BIOCONDUCTOR packages
-source('https://bioconductor.org/biocLite.R')
+source("https://bioconductor.org/biocLite.R")
 biocLite("flowCore")
 biocLite("flowQ")
 biocLite("flowViz")
